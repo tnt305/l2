5e3d0828f384090604200382e08f234a124bef01
Thien Dang <110881337+tnt305@users.noreply.github.com>
2024-04-13 02:21:32 +0700

    Update videos2images.py

--- videos2images.py
+++ videos2images.py
@@ -57,7 +57,7 @@ def gen_image(video_folder, image_folder):
         
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--video_folder', default='/kaggle/working/aicity2024/aicity2024_track5_train/videos', \
+    parser.add_argument('--video_folder', default='/kaggle/working/aicity2024/aicity2024_track5_train/videos/', \
                         required=False ,help='path to aicity challenge videos folder')
     parser.add_argument('--save_folder', default='/kaggle/working/aicity2024/aicity2024_track5_images/', \
                         required=False ,help='path to save images')
