0404be3cd45910426cee397fadafaf1d8bc76e21
Thien Dang <110881337+tnt305@users.noreply.github.com>
2024-04-14 01:36:04 +0700

    Update matcher.py

--- helmet_detection_for_motorcyclists/effdet/object_detection/matcher.py
+++ helmet_detection_for_motorcyclists/effdet/object_detection/matcher.py
@@ -162,31 +162,59 @@ class Match(object):
     def _reshape_and_cast(self, t):
         return torch.reshape(t, [-1]).long()
 
+    # def gather_based_on_match(self, input_tensor, unmatched_value, ignored_value):
+    #     """Gathers elements from `input_tensor` based on match results.
+
+    #     For columns that are matched to a row, gathered_tensor[col] is set to input_tensor[match_results[col]].
+    #     For columns that are unmatched, gathered_tensor[col] is set to unmatched_value. Finally, for columns that
+    #     are ignored gathered_tensor[col] is set to ignored_value.
+
+    #     Note that the input_tensor.shape[1:] must match with unmatched_value.shape
+    #     and ignored_value.shape
+
+    #     Args:
+    #         input_tensor: Tensor to gather values from.
+    #         unmatched_value: Constant tensor value for unmatched columns.
+    #         ignored_value: Constant tensor value for ignored columns.
+
+    #     Returns:
+    #         gathered_tensor: A tensor containing values gathered from input_tensor.
+    #             The shape of the gathered tensor is [match_results.shape[0]] + input_tensor.shape[1:].
+    #     """
+    #     device = torch.device("cuda")
+    #     ss = torch.stack([ignored_value, unmatched_value]).to(device)
+    #     input_tensor = torch.cat([ss, input_tensor], dim=0).to(device)
+    #     gather_indices = torch.clamp(self.match_results + 2, min=0).to(device)
+    #     gathered_tensor = torch.index_select(input_tensor, 0, gather_indices)
+    #     return gathered_tensor
     def gather_based_on_match(self, input_tensor, unmatched_value, ignored_value):
         """Gathers elements from `input_tensor` based on match results.
-
+    
         For columns that are matched to a row, gathered_tensor[col] is set to input_tensor[match_results[col]].
         For columns that are unmatched, gathered_tensor[col] is set to unmatched_value. Finally, for columns that
         are ignored gathered_tensor[col] is set to ignored_value.
-
+    
         Note that the input_tensor.shape[1:] must match with unmatched_value.shape
         and ignored_value.shape
-
+    
         Args:
             input_tensor: Tensor to gather values from.
             unmatched_value: Constant tensor value for unmatched columns.
             ignored_value: Constant tensor value for ignored columns.
-
+    
         Returns:
             gathered_tensor: A tensor containing values gathered from input_tensor.
                 The shape of the gathered tensor is [match_results.shape[0]] + input_tensor.shape[1:].
         """
         device = torch.device("cuda")
-        ss = torch.stack([ignored_value, unmatched_value]).to(device)
+        # Create ss tensor with the same shape as input_tensor, but filled with ignored_value
+        ss = torch.full_like(input_tensor, ignored_value).to(device)
+        # Concatenate ss and input_tensor along the first dimension
         input_tensor = torch.cat([ss, input_tensor], dim=0).to(device)
-        gather_indices = torch.clamp(self.match_results + 2, min=0).to(device)
+        gather_indices = torch.clamp(self.match_results + 1, min=0).to(device)
         gathered_tensor = torch.index_select(input_tensor, 0, gather_indices)
         return gathered_tensor
+
 
 
 class Matcher(object):
